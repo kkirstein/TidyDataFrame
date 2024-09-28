8ad9c05d0ea3f54be5168b9aa9e09971d479d69a
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2024-09-28 18:37:15 +0200

    add stubs for pivoting methods

---
+++ TidyDataFrame.Extensions/Pivoting.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Analysis;
+using System.Runtime.CompilerServices;
+
+namespace TidyDataFrame.Extensions
+{
+
+    public static class Pivoting
+    {
+
+        public static DataFrame PivotLonger(this DataFrame df, List<string> cols, string names_to = "Names", string values_to = "Values")
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DataFrame PivotWider(this DataFrame df, List<string> cols, string names_from)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
