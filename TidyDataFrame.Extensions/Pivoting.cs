3372783024fa4fb6c86e801154f89884e57dc216
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2024-09-30 09:06:24 +0200

    fix .net naming convention

--- TidyDataFrame.Extensions/Pivoting.cs
+++ TidyDataFrame.Extensions/Pivoting.cs
@@ -7,12 +7,12 @@ namespace TidyDataFrame.Extensions
     public static class Pivoting
     {
 
-        public static DataFrame PivotLonger(this DataFrame df, List<string> cols, string names_to = "Names", string values_to = "Values")
+        public static DataFrame PivotLonger(this DataFrame df, List<string> cols, string namesTo = "Names", string valuesTo = "Values")
         {
             throw new NotImplementedException();
         }
 
-        public static DataFrame PivotWider(this DataFrame df, List<string> cols, string names_from)
+        public static DataFrame PivotWider(this DataFrame df, List<string> cols, string namesFrom)
         {
             throw new NotImplementedException();
         }
