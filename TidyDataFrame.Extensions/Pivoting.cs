13dc964acde69202cea304305730b02758cb8306
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2024-10-01 13:59:16 +0200

    format source

--- TidyDataFrame.Extensions/Pivoting.cs
+++ TidyDataFrame.Extensions/Pivoting.cs
@@ -3,10 +3,8 @@ using System.Runtime.CompilerServices;
 
 namespace TidyDataFrame.Extensions
 {
-
     public static class Pivoting
     {
-
         public static DataFrame PivotLonger(this DataFrame df, List<string> cols, string namesTo = "Names", string valuesTo = "Values")
         {
             return TidyDataFrame.Pivoting.ToLonger(df, cols, namesTo, valuesTo);
